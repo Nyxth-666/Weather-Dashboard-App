a9e9aca074334882075fbf22c216d16e1e6b6b41
Jonel <jonelpronton06@gmail.com>
2026-04-10 17:39:55 +0800

    function of component 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import Component1 from "./components/Component1";
-import Component2 from "./components/Component2";
-import Component4 from "./components/Component4";
-import "./App.css";
 
+import Component3 from "./components/Component3";
 const App = () => {
   return (
-    <>
-      <div className="main-app">
-        <div className="app-wrapper">
-          <Component2 />
-          <div className="app-info">
-            <Component1 />
-          </div>
-          <Component4 />
-        </div>
-      </div>
-    </>
+    <div>
+      <Component1 />
+    </div>
   );
 };
 
