cf878bfe5e115bbee360ca080f1f6d43a74fc32e
NielEsguerra <nathanielesguerra000@gmail.com>
2026-04-11 18:31:28 +0800

    Update Component2.jsx

--- src/components/Component2.jsx
+++ src/components/Component2.jsx
@@ -1,6 +1,36 @@
 import '../styles/Component2.css';
 
 const Component2 = () => {
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState(null);
+    const [error, setError] = useState('');
+
+
+  const API_KEY = "YOUR_API_KEY";
+
+  const fetchWeather = async () => {
+    if (!city) return;
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const data = await response.json();
+      setWeather(data);
+    } catch (err) {
+      setError(err.message);  
+      setWeather(null);
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchWeather();
+  };
     return (
         <div className="searchcontainer">
             <div className="searchelements">
