6e9cd67a391112a1aab14554f718e4c87b900613
Nyxth-666 <joshuabarotea06@gmail.com>
2026-04-11 12:49:13 +0800

    new shit

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -62,38 +62,44 @@ const Component1 = () => {
   }, []);
 
   return (
-    <div className="searched-weather box">
-      <div className="searched-wrapper">
-        <div className="city-info">
-          <img src="/src/assets/rain.png" />
-          <div className="info-label">
-            <div className="loc-date">
-              <label className="city">Batangas, PH</label>
-              <label className="date">Tuesday. April 7, 2026</label>
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {weatherData && (
+        <div className="searched-weather box">
+          <div className="searched-wrapper">
+            <div className="city-info">
+              <img src="/src/assets/rain.png" />
+              <div className="info-label">
+                <div className="loc-date">
+                  <label className="city">Batangas, PH</label>
+                  <label className="date">Tuesday. April 7, 2026</label>
+                </div>
+                <div className="temp-label">
+                  <label className="temp">29°C</label>
+                  <label className="temp-label">Few Clouds</label>
+                </div>
+              </div>
             </div>
-            <div className="temp-label">
-              <label className="temp">29°C</label>
-              <label className="temp-label">Few Clouds</label>
-            </div>
-          </div>
-        </div>
 
-        <div className="city-stats">
-          <div className="stat-box">
-            <p className="stat-title">HUMIDITY</p>
-            <h3>77%</h3>
-          </div>
-          <div className="stat-box">
-            <p className="stat-title">WIND</p>
-            <h3>1.86 m/s</h3>
-          </div>
-          <div className="stat-box">
-            <p className="stat-title">FEELS LIKE</p>
-            <h3>34°C</h3>
+            <div className="city-stats">
+              <div className="stat-box">
+                <p className="stat-title">HUMIDITY</p>
+                <h3>77%</h3>
+              </div>
+              <div className="stat-box">
+                <p className="stat-title">WIND</p>
+                <h3>1.86 m/s</h3>
+              </div>
+              <div className="stat-box">
+                <p className="stat-title">FEELS LIKE</p>
+                <h3>34°C</h3>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
