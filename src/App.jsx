6bb82e150ea8454f4bd67185db05ad2da7971b98
James <jamesorte089@gmail.com>
2026-04-11 13:53:27 +0800

    add component 4 jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Component1 from "./components/Component1";
 import Component2 from "./components/Component2";
+import Component4 from "./components/Component4";
 
 const App = () => {
   return (
     <div>
       <Component2 />
       <Component1 />
+      <Component4 />
     </div>
   );
 };
